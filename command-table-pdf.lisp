036272b951d03f50de8337027cc7ae290c05a275
Olaf Merkert <olaf@m-merkert.de>
2011-05-22 17:27:49 +0200

    Generate a pdf file with a keyboard reference

---
+++ command-table-pdf.lisp
@@ -0,0 +1,60 @@
+(in-package #:dcs-commands)
+
+(defparameter *text-font* (pdf:get-font "times-roman"))
+(defparameter *command-font* (pdf:get-font "courier"))
+(defparameter *test-output* #P "/home/olaf/test-output.pdf")
+
+(defconfun command-table-pdf (file &optional (all nil))
+  (let (row-parity
+        content)
+    (tt:with-document ()
+      (setf content
+            (tt:compile-text (:font "helvetica"
+                                    :font-size 9)
+              (tt:paragraph (:font-size 24 :h-align :center)
+                (tt:put-string "DCS Keyboard reference"))
+              (tt:table (:col-widths '(250 70) :splittable-p t)
+                (dolist (category (sorted-commands (not all) config))
+                  (tt:row #1=(:background-color (if (notf row-parity)
+                                                    :gray :white))
+                          (tt:cell (:col-span 2)
+                            (tt:paragraph (:font "times-bold" :h-align :center :font-size 11)
+                              (tt:put-string (car category)))))
+                  (dolist (command (cdr category))
+                    (tt:row #1#
+                      (tt:cell ()
+                        (tt:paragraph (:font *text-font* :font-size 9)
+                          (tt:put-string (command-name command))))
+                      (tt:cell ()
+                        (tt:paragraph (:font *command-font* :font-size 9)
+                          (tt:put-string (command-combo-emacs command))))))))))
+      #|(tt:draw-multicolumn content :columns 3 :orientation
+      :landscape :margins '(20 20 20 20)
+      :column-space 10)|#
+      (tt:draw-pages content :orientation :portrait :margins '(20 30 20 40))
+      (pdf:write-document file))))
+
+(in-package #:tt)
+(export '(draw-multicolumn with-page))
+(defun draw-multicolumn (content &rest args
+                         &key (columns 2)
+                         (column-space 0)
+                         (size *default-page-size*)
+                         (orientation *default-page-orientation*)
+                         bounds margins
+                         &allow-other-keys)
+  (with-quad (left-margin top-margin right-margin bottom-margin) margins
+    (let* ((bounds (or bounds (compute-page-bounds size orientation)))
+           (width (aref bounds 2))
+           (height (aref bounds 3))
+           (column-width (floor (/ (- width left-margin right-margin
+                                      (* column-space columns))
+                                    columns)))
+           (column-height (- height top-margin bottom-margin)))
+      (dotimes (i columns)
+        (draw-block content (+ left-margin (* i (+ column-width column-space)))
+                    (- height top-margin) column-width column-height)))))
+;;; todo fix this stuff to work properly
+;;; current issues:
+;;; * orientation is not honoured in the output
+;;; * multiple pages do not work
--- dcs-commands.lisp
+++ dcs-commands.lisp
@@ -1,7 +1,7 @@
 (in-package #:dcs-commands)
 
 (defparameter *config-path* #P"/home/olaf/lisp/hotas-programming/default.lua")
-(defparameter *last-config* nil)
+(defvar *last-config* nil)
 
 (defun load-config (file)
   (setf *last-config*
@@ -9,7 +9,9 @@
      (parse-lua-stream-to-lisp stream))))
 
 (defmacro defconfun (name args &body body)
-  `(defun ,name (,@args &optional (config *last-config*))
+  `(defun ,name (,@args
+                 ,@(unless (member '&optional args) `(&optional))
+                 (config *last-config*))
      ,@body))
 
 (defconfun modifier-list ()
@@ -28,12 +30,48 @@
    (list :emacs "rW-" "W-" "rC-" "C-" "rM-" "M-" "rS-" "S-")
    (list :logi "RGUI" "LGUI" "RCTRL" "LCTRL" "RALT" "LALT" "RSHIFT" "LSHIFT")))
 
-(defun translate-modifiers (from to mod)
+(defun translate-modifier (from to mod)
   (let ((i (position mod (assoc from *modifiers*) :test #'equal)))
     (nth i (assoc to *modifiers*))))
+
+(defun translate-modifiers (from to mods)
+  (mapcar (lambda (x) (translate-modifier from to x)) mods))
 
 (defun sort-modifiers (dialect mods)
   (let ((mod-list (assoc1 dialect *modifiers*)))
    (sort mods
          #'<
          :key (lambda (x) (position x mod-list :test #'equal)))))
+
+(defconfun sorted-commands (&optional (only-assigned t))
+  (let* ((filtered-config
+           (if only-assigned
+               (remove-if-not (lambda (x) (assoc1 :combos x)) (command-list config))
+               (command-list config)))
+         (grouped-config
+          (group-by (lambda (x) (assoc1 :category x)) filtered-config))
+         (sorted-config
+          (sort
+           (mapcar (lambda (x)
+                     (cons (car x)
+                           (sort (cdr x) #'string<=
+                                 :key (lambda (y) (assoc1 :name y)))))
+                   grouped-config)
+           #'string<=
+           :key #'car)))
+    sorted-config))
+
+(defun command-name (command)
+  (assoc1 :name command))
+
+(defun command-combo (command)
+  (aif (assoc1 :combos command)
+       (cons (assoc1 :key (car it))
+             (assoc1 :reformers (car it)))))
+
+(defun command-combo-emacs (command)
+  (aif (command-combo command)
+       (let ((key (car it))
+             (mods (sort-modifiers :emacs (translate-modifiers :dcs :emacs (cdr it)))))
+         (format nil "~{~A~}~A" mods key))
+       ""))
