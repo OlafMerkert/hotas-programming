d7b579d869675e590f2b10c39fd50ebc6359e99c
Olaf Merkert <olaf@m-merkert.de>
2011-11-16 02:07:01 +0100

    fine tuning the tex output function

--- command-table-tex.lisp
+++ command-table-tex.lisp
@@ -3,7 +3,11 @@
 (defparameter *test-output* #P "/home/olaf/test-output.tex")
 
 (defun tex-escape (string)
-  (string-replace-all "&" string "\\&"))
+  (dolist (rl '(("&" . "\\&")
+                ("%" . "\\%")))
+    
+    (setf string (string-replace-all (car rl) string (cdr rl))))
+  string)
 
 (defun add-modifiers (commands)
   (flet ((modi (ref name)
@@ -19,32 +23,6 @@
                 (modi "LWin" "Left Windows")
                 (modi "RWin" "Right Windows"))
           commands)))
-
-(defconfun command-table-tex (file &optional (aircraft "A-10C") all)
-  (with-tex-output file
-    (str "\\documentclass[a4paper,landscape]{article}")
-    (str "\\usepackage[margin=1.5cm]{geometry}")
-    #|(tex "usepackage" "supertabular")|#
-    (tex "usepackage" "multicol")
-    (tex "newcommand" "\\stab" "\\begin{tabular}{|p{5.5cm}|p{2cm}|}\\hline")
-    (tex "newcommand" "\\etab" "\\end{tabular}")
-    (tex "begin" "document")
-    
-    (tex "twocolumn")
-    (tex "centerline" (format nil "\\Large \\bfseries DCS ~A Keyboard Bindings" aircraft))
-    (tex "begin" "multicols" 3)
-    (dolist (category (add-modifiers (sorted-commands (not all) config)))
-      (tex "indent")
-      (str "{\\bfseries \\large ~A}\\\\[8pt]~%" (tex-escape (car category)))
-      (tex "stab")
-      (dolist (command (cdr category))
-        (row (tex-escape (command-name command))
-             (format nil "{\\verb|~A|}" (command-combo-emacs command)))
-        (tex "hline"))
-      (tex "etab")
-      (str "~%"))
-    (tex "end" "multicols")
-    (tex "end" "document")))
 
 (defmacro! with-tex-output (file &body body)
   `(with-open-file (,g!tex-stream ,file :direction :output :if-exists :supersede)
@@ -66,6 +44,34 @@
               (format ,g!tex-stream"~{~A~^ & ~} \\\\~%" ,g!cells)))
        ,@body)))
 
+(defconfun command-table-tex (file &optional (aircraft "A-10C") all)
+  (with-tex-output file
+    (str "\\documentclass[a4paper,landscape]{article}")
+    (str "\\usepackage[margin=1cm]{geometry}")
+    (tex "usepackage" "multicol")
+    (tex "usepackage" "stabular")
+    (tex "newcommand" "\\stab"  "\\begin{stabular}{|p{5cm}|p{2cm}|}\\hline")
+    (tex "newcommand" "\\etab" "\\end{stabular}")
+    (tex "setlength" "\\parindent" "0pt")
+    (tex "begin" "document")
+    
+    (tex "centerline" (format nil "\\huge \\bfseries DCS ~A Keyboard Bindings" aircraft))
+    (tex "raggedcolumns")
+    (tex "raggedbottom")
+    (tex "begin" "multicols" 3)
+    (dolist (category (add-modifiers (sorted-commands (not all) config)))
+      (tex "medskip")
+      (str "{\\bfseries \\large ~A}\\\\[0.2cm]~%" (tex-escape (car category)))
+      (tex "stab")
+      (dolist (command (cdr category))
+        (row (tex-escape (command-name command))
+             (format nil "{\\verb|~A|}" (command-combo-emacs command)))
+        (tex "hline"))
+      (tex "etab")
+      (str "~%"))
+    (tex "end" "multicols")
+    (tex "end" "document")))
+
 (defun load-lua-generate-tex (lua aircraft)
   (let* ((lua-file (merge-pathnames lua #P "/home/olaf/Projekte/hotas-programming/"))
          (tex-file (make-pathname :type "tex" :defaults lua-file)))
@@ -77,4 +83,6 @@
 
 (defun samples ()
   (load-lua-generate-tex #P"a10-v1110-default.lua" "A-10C")
-  #|(load-lua-generate-tex "ka-50-v2-default.lua" "Ka-50 v2")|#)
+  (load-lua-generate-tex "ka-50-v2-default.lua" "Ka-50 v2"))
+
+(defparameter *ka-50-config* (merge-pathnames "ka-50-v2-default.lua" #P"/home/olaf/Projekte/hotas-programming/"))
